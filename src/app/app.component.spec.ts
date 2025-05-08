79767896c665b15690f7af3dc1f779876bc12e35
Matthew Salerno <msalerno@dtcc.com>
2025-05-08 13:06:43 -0400

    init 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'star-coders' title`, () => {
+  it(`should have the 'star-coders-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('star-coders');
+    expect(app.title).toEqual('star-coders-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, star-coders');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, star-coders-angular');
   });
 });
